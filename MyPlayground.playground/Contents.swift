972481fa23d2d12c7826c8882c162da5d5978963
Cos Kim <junzerokim@gmail.com>
2023-07-05 23:33:28 +0900

    Swift Property Observer

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,45 +1,14 @@
 import UIKit
 
-// Struct
-struct YoutuberStruct {
-    var name: String
-    var subscribersCount: Int
-}
-
-var devJun = YoutuberStruct(name: "junyoung", subscribersCount: 100)
-
-var devJunClone = devJun
-
-print("=== Struct ===")
-
-print("값 넣기 전 devJunClone.name: \(devJunClone.name)")
-
-devJunClone.name = "jun"
-
-print("값 넣은 후 devJunClone.name: \(devJunClone.name)")
-print("값 넣은 후 devJun.name: \(devJun.name)")
-
-// Class
-class YoutuberClass {
-    var name: String
-    var subscribersCount: Int
-    // up memory
-    init(name: String, subscribersCount: Int) {
-        self.name = name
-        self.subscribersCount = subscribersCount
+// Property Observer
+var myAge = 0 {
+    willSet {
+        print("값이 설정될 예정 myAge: \(myAge)")
+    }
+    didSet {
+        print("값이 설정되었다 myAge: \(myAge)")
     }
 }
 
-var junyoung = YoutuberClass(name: "junyoung", subscribersCount: 1000)
-
-var junyoungClone = junyoung
-
-
-print("=== Class ===")
-
-print("값 넣기 전 junyoungClone.name: \(junyoungClone.name)")
-
-junyoungClone.name = "kimjunyoung"
-
-print("값 넣은 후 junyoungClone.name: \(junyoungClone.name)")
-print("값 넣은 후 junyoung.name: \(junyoung.name)")
+myAge = 10
+myAge = 20
