9060fb69c377634268c424f112d943d7a2005d55
Cos Kim <junzerokim@gmail.com>
2023-07-05 22:02:59 +0900

    Swift Optional

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,36 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Optional
+var someVariable : Int? = nil
+
+if someVariable == nil {
+    someVariable = 90
+}
+
+if let otherVariable = someVariable {
+    print("Unwrapped. otherVariable: \(otherVariable)")
+} else {
+    print("Has a value")
+}
+
+someVariable = nil
+
+let myValue = someVariable ?? 10
+print("myValue: \(myValue)")
+
+var firstValue: Int? = 30
+var secondValue: Int? = 50
+
+print("firstValue: \(firstValue)")
+print("secondValue: \(secondValue)")
+
+unWrap(firstValue)
+unWrap(secondValue)
+
+func unWrap(_ parameter: Int?) {
+    print("unwrap() called")
+    guard let unWrappedParam = parameter else {
+        return
+    }
+    print("unWrappedParam: \(unWrappedParam)")
+}
